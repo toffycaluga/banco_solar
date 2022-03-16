d0e4a8dd280144b57cf8783c804202cf17f9dcd8
Toffy_Caluga <95626402+toffycaluga@users.noreply.github.com>
2022-03-16 16:20:39 -0300

    Update db.js

--- utilities/db.js
+++ utilities/db.js
@@ -21,55 +21,83 @@ export async function leerUsuario() {
         client.release();
         return rows;
     } catch (err) {
-        console.log(err);
+        manejoError(err);
         client.release();
     }
 }
 export async function insertarUsuario(nombre, balance) {
     const client = await pool.connect();
-    try {
-        const { row } = await client.query({
-            text: `insert into usuarios(nombre,balance) values ($1,$2) returning *`,
-            values: [nombre, balance],
-            name: 'insert-de-los-usuarios'
-        });
-        console.log(` ${nombre} agregado con exito!`);
-        client.release();
-    } catch (err) {
-        console.log(err);
-        client.release();
+    if (balance >= 0) {
+        try {
+            const { row } = await client.query({
+                text: `insert into usuarios(nombre,balance) values ($1,$2) returning *`,
+                values: [nombre, balance],
+                name: 'insert-de-los-usuarios'
+            });
+            console.log(` ${nombre} agregado con exito!`);
+            client.release();
+        } catch (err) {
+            manejoError(err);
+            client.release();
+        }
+    } else {
+        client.release;
+        return 'balance debe ser mayor a 0'
     }
+
 
 }
 export async function editarUsuarios(id, nombre, balance) {
     const client = await pool.connect();
+    if (balance >= 0) {
+        try {
+            const { row } = await client.query({
+                text: `update usuarios set nombre=$2, balance=$3 where id=$1 returning *`,
+                values: [id, nombre, balance],
+                name: 'update-de-los-usuarios'
+            });
+            console.log(` ${nombre} editado con exito!`);
+            client.release();
+            return true;
+        } catch (err) {
+            manejoError(err);
+            client.release();
+        }
+    } else {
+        client.release();
+        return 'balance debe ser  mayor que 0'
+    }
+
+}
+async function eliminarTransferencia(id) {
+    const client = await pool.connect();
     try {
-        const { row } = await client.query({
-            text: `update usuarios set nombre=$2, balance=$3 where id=$1 returning *`,
-            values: [id, nombre, balance],
-            name: 'update-de-los-usuarios'
-        });
-        console.log(` ${nombre} editado con exito!`);
+        const { rows } = await client.query({
+            text: `delete from transferencias where receptor=$1 or emisor=$1;`,
+            values: [id],
+            name: 'delete-por-id'
+        })
         client.release();
     } catch (err) {
-        console.log(err);
+        manejoError(err);
         client.release();
     }
 
 }
-
 export async function eliminarUsuario(id) {
     const client = await pool.connect();
     try {
+        await eliminarTransferencia(id);
         const { row } = await client.query({
-            text: `delete from usuarios where id=$1 returning *`,
+            text: `
+            delete from usuarios where id=$1 returning *;`,
             values: [id],
-            name: 'delete-por-rut'
+            name: 'delete-por-id'
         })
         console.log(`eliminado con exito`);
         client.release();
     } catch (err) {
-        console.log(err);
+        manejoError(err);
         client.release();
     }
 
@@ -80,18 +108,34 @@ export async function nuevaTransferencia(emisor, receptor, Monto) {
     const datosEmisor = await buscarId(emisor);
     const datosReceptor = await buscarId(receptor);
     const monto = parseInt(Monto)
-    try {
-        const { rows } = await client.query({
-            text: `insert into transferencias (emisor,receptor,monto) values ($1,$2,$3)`,
-            values: [datosEmisor[0].id, datosReceptor[0].id, monto],
-            name: 'insert-de-las-tranferencias'
-        })
-        await updateBalance(datosEmisor[0].id, datosEmisor[0].balance, (-monto));
-        await updateBalance(datosReceptor[0].id, datosReceptor[0].balance, (monto));
-        client.release()
-    } catch (err) {
-        console.log(err);
+    if (datosEmisor[0].id != datosReceptor[0].id) {
+        if (monto >= 0) {
+            if (monto < datosEmisor[0].balance) {
+                try {
+                    const { rows } = await client.query({
+                        text: `insert into transferencias (fecha,emisor,receptor,monto) values (now(),$1,$2,$3)`,
+                        values: [datosEmisor[0].id, datosReceptor[0].id, monto],
+                        name: 'insert-de-las-tranferencias'
+                    })
+                    await updateBalance(datosEmisor[0].id, datosEmisor[0].balance, (-monto));
+                    await updateBalance(datosReceptor[0].id, datosReceptor[0].balance, (monto));
+                    client.release()
+                    return true
+                } catch (err) {
+                    console.log(err);
+                    client.release();
+                }
+            } else {
+                client.release()
+                return 'monto excede la cantidad permitida';
+            }
+        } else {
+            client.release();
+            return 'valor debe ser mayor que 0'
+        }
+    } else {
         client.release();
+        return 'no puede realizar transferencia entre la misma'
     }
 }
 
